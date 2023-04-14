0f4af28e3b9555421643b18cd6f85c055766655d
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-14 23:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function displayTimer{
+	var date=new Date();
+	var time=date.toLocalTimeString();
+	var options={day:'numeric' ,month:'long', year:'numeric'};
+	var today=date.toLocalDateString(undefined , options);	
+	document.getElementById("timer").innerHTML=today + time;
+}
+setInterval(displayTimer, 1000);
