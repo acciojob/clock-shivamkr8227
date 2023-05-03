847451ce05cabdbbfbd3b57158d85585e38d3bd2
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-03 17:31:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 //your JS code here. If required.
-function displayTimer(){
-	var date=new Date();
-	var time=date.toLocaleTimeString();
-	var options={day:'numeric' ,month:'long', year:'numeric'};
-	var today=date.toLocaleDateString(undefined , options);	
-	document.getElementById("timer").innerHTML=today + time;
+// function displayTimer(){
+// 	var date=new Date();
+// 	var time=date.toLocaleTimeString();
+// 	var options={day:'numeric' ,month:'long', year:'numeric'};
+// 	var today=date.toLocaleDateString(undefined , options);	
+// 	document.getElementById("timer").innerHTML=today + time;
+// }
+// setInterval(displayTimer, 1000);
+
+const time=new Date();
+let daystring = `${time.getMonth() +1}/${time.getDate}/${time.getFullYear}`;
+let hr=time.getHours();
+let timestring="";
+if(hr>=12)
+{
+	timestring=`hr-12: ${time.getMinutes}:${time.getSeconds} PM`;
 }
-setInterval(displayTimer, 1000);
+else{
+	timestring=`hr: ${time.getMinutes}:${time.getSeconds} AM`;
+}
+let ptimer=document.getElementById("timer");
+ptimer.innerHTML=daystring+timestring;
